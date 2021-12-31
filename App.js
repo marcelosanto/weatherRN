bd85c057aff1e3d0199f39e2aa5fc492d84efecf
Marcelo Santos <rocha.mer21@gmail.com>
2021-12-30 21:46:39 -0300

    add react vector icons

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import Home from './src/screens/Home'
+import { ThemeProvider } from 'styled-components'
+import { useColorScheme } from 'react-native'
+
+import themes from './src/themes/index.js'
 
 const App = () => {
-  return <Home />
+  //dark, light, null
+  const deviceTheme = useColorScheme()
+  const theme = themes[deviceTheme] || theme.dark
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  )
 }
 
 export default App
---
+++ src/utils/utils.js
@@ -0,0 +1,112 @@
+const data = {
+  temp: 25,
+  date: '30/12/2021',
+  time: '20:48',
+  condition_code: '29',
+  description: 'Parcialmente nublado',
+  currently: 'noite',
+  cid: '',
+  city: 'Serra, ES',
+  img_id: '29n',
+  humidity: 83,
+  wind_speedy: '4.12 km/h',
+  sunrise: '05:03 am',
+  sunset: '06:23 pm',
+  condition_slug: 'cloud',
+  city_name: 'Serra',
+  forecast: [
+    {
+      date: '30/12',
+      weekday: 'Qui',
+      max: 28,
+      min: 22,
+      description: 'Chuvas esparsas',
+      condition: 'rain',
+    },
+    {
+      date: '31/12',
+      weekday: 'Sex',
+      max: 28,
+      min: 21,
+      description: 'Chuvas esparsas',
+      condition: 'rain',
+    },
+    {
+      date: '01/01',
+      weekday: 'Sáb',
+      max: 28,
+      min: 22,
+      description: 'Chuvas esparsas',
+      condition: 'rain',
+    },
+    {
+      date: '02/01',
+      weekday: 'Dom',
+      max: 28,
+      min: 22,
+      description: 'Tempo nublado',
+      condition: 'cloudly_day',
+    },
+    {
+      date: '03/01',
+      weekday: 'Seg',
+      max: 30,
+      min: 22,
+      description: 'Tempo nublado',
+      condition: 'cloudly_day',
+    },
+    {
+      date: '04/01',
+      weekday: 'Ter',
+      max: 29,
+      min: 22,
+      description: 'Chuvas esparsas',
+      condition: 'rain',
+    },
+    {
+      date: '05/01',
+      weekday: 'Qua',
+      max: 30,
+      min: 23,
+      description: 'Tempo nublado',
+      condition: 'cloudly_day',
+    },
+    {
+      date: '06/01',
+      weekday: 'Qui',
+      max: 30,
+      min: 23,
+      description: 'Chuvas esparsas',
+      condition: 'rain',
+    },
+    {
+      date: '07/01',
+      weekday: 'Sex',
+      max: 30,
+      min: 22,
+      description: 'Chuvas esparsas',
+      condition: 'rain',
+    },
+    {
+      date: '08/01',
+      weekday: 'Sáb',
+      max: 31,
+      min: 23,
+      description: 'Tempo nublado',
+      condition: 'cloudly_day',
+    },
+  ],
+}
+
+/* storm - tempestade
+snow - neve
+hail - granizo
+rain - chuva
+fog - neblina
+clear_day - dia limpo
+clear_night - noite limpa
+cloud - nublado
+cloudly_day - nublado de dia
+cloudly_night - nublado de noite
+none_day - erro ao obter mas está de dia
+none_night - erro ao obter mas está de noite */
